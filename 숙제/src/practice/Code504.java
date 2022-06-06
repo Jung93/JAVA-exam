46683ff9f175fbd746af0e69e6147b8868d035d1
Jung93 <orijung93@gmail.com>
2022-06-06 09:54:16 +0900

    jungol code

---
+++ 숙제/src/practice/Code504.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Code504 {
+	public static void main(String[] args) {
+		System.out.println("(@) (@)");
+		System.out.println("(=^.^=)");
+		System.out.println("(-m-m-)");
+	}
+}
---
+++ 숙제/src/practice/Code505.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Code505 {
+
+	public static void main(String[] args) {
+		System.out.println("I can program well.");
+		System.out.println("Dreams come true.");
+	}
+}
