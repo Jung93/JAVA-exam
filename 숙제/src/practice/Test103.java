1c5173294d0c5a2445e021885e232f16ebe525b0
Jung93 <orijung93@gmail.com>
2022-06-08 19:32:05 +0900

    jungol test

---
+++ 숙제/src/practice/Test103.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Test103 {
+	public static void main(String[] args) {
+		System.out.println("TTTTTTTTTT");
+		System.out.println("TTTTTTTTTT");
+		System.out.println("    TT    ");
+		System.out.println("    TT    ");
+		System.out.println("    TT    ");		
+	}
+}
