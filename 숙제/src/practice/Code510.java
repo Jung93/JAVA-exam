5d5386186e8567a39f172a71e97c5f6905501045
Jung93 <orijung93@gmail.com>
2022-06-13 10:09:38 +0900

    jungol code510

---
+++ 숙제/src/practice/Code510.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Code510 {
+	public static void main(String[] args) {
+		int num1 = -1;
+		int num2 = 100;
+		System.out.println(num1);
+		System.out.println(num2);
+	}
+}
