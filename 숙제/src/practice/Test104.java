78b78ff401c4559547f8dce276a9309e494b132c
Jung93 <orijung93@gmail.com>
2022-06-13 09:46:59 +0900

    jungol test

---
+++ 숙제/src/practice/Test104.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Test104 {
+	public static void main(String[] args) {
+		int kor = 90;
+		int mat = 80;
+		int eng = 100;
+		int sum = kor+mat+eng;
+		System.out.println("kor "+kor);
+		System.out.println("mat "+mat);
+		System.out.println("eng "+eng);
+		System.out.println("sum "+sum);
+	}
+}
